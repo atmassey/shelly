a74a6d65772ead20b2990bc147eb45c010a26aa4
Austin Massey <austinmassey.am@gmail.com>
2024-12-15 17:37:03 -0600

    Add functions to reboot device and retrieve OTA information

--- gen1/common.go
+++ gen1/common.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Provides basic information about the Shelly device.
 func GetShelly(IP string) (Shelly, error) {
 	var shelly Shelly
 	req, err := http.NewRequest("GET", "http://"+IP+"/shelly", nil)
@@ -21,4 +22,43 @@ func GetShelly(IP string) (Shelly, error) {
 		return Shelly{}, err
 	}
 	return shelly, nil
+}
+
+// When requested the device will reboot.
+func Reboot(IP string) error {
+	req, err := http.NewRequest("GET", "http://"+IP+"/reboot", nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+// Provides information about the current firmware version and the ability to trigger and over-the-air update.
+func GetOta(IP string) (Ota, error) {
+	var ota Ota
+	req, err := http.NewRequest("GET", "http://"+IP+"/ota", nil)
+	if err != nil {
+		return Ota{}, err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return Ota{}, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return Ota{}, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&ota); err != nil {
+		return Ota{}, err
+	}
+	return ota, nil
 }
--- gen1/structs.go
+++ gen1/structs.go
@@ -8,3 +8,14 @@ type Shelly struct {
 	LongId       int    `json:"longid"`
 	Discoverable bool   `json:"discoverable"`
 }
+
+type Ota struct {
+	Status      string `json:"status"`
+	HasUpdate   bool   `json:"has_update"`
+	New_Version string `json:"new_version"`
+	Old_Version string `json:"old_version"`
+}
+
+type Ota_Check struct {
+	Status string `json:"status"`
+}
